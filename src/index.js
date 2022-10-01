9f28ed37b3a9f8df4669ad5cd0d2f8723652eed7
Anastasiia <guba.anastaysha@gmail.com>
2022-10-01 12:39:19 +0300

    file is updated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import * as readMoreBtn from './js/read-more-btn';
+// import * as images from './js/images';
--- src/js/read-more-btn.js
+++ src/js/read-more-btn.js
@@ -1,20 +1,41 @@
-const readMoreBtn = document.querySelector('.read-more-btn');
-const readLessBtn = document.querySelector('.read-less-btn');
-const moreText = document.querySelector('.read-more-text');
-
-readMoreBtn.addEventListener('click', onReadMoreBtnClick);
-readLessBtn.addEventListener('click', onReadLessBtnClick);
-
+// FOR SECTION ABOUT
 const mainClass = 'invisible';
 
-function onReadMoreBtnClick() {
-  moreText.classList.toggle(`${mainClass}`);
-  readMoreBtn.classList.toggle(`${mainClass}`);
-  readLessBtn.classList.toggle(`${mainClass}`);
+const readMoreBtnAbout = document.querySelector('.read-more-btn__about');
+const readLessBtnAbout = document.querySelector('.read-less-btn__about');
+const moreTextAbout = document.querySelector('.read-more-text__about');
+
+readMoreBtnAbout.addEventListener('click', onReadMoreBtnAboutClick);
+readLessBtnAbout.addEventListener('click', onReadLessBtnAboutClick);
+
+function onReadMoreBtnAboutClick(e) {
+  moreTextAbout.classList.toggle(`${mainClass}`);
+  readMoreBtnAbout.classList.toggle(`${mainClass}`);
+  readLessBtnAbout.classList.toggle(`${mainClass}`);
 }
 
-function onReadLessBtnClick() {
-  moreText.classList.toggle(`${mainClass}`);
-  readMoreBtn.classList.toggle(`${mainClass}`);
-  readLessBtn.classList.toggle(`${mainClass}`);
+function onReadLessBtnAboutClick() {
+  moreTextAbout.classList.toggle(`${mainClass}`);
+  readMoreBtnAbout.classList.toggle(`${mainClass}`);
+  readLessBtnAbout.classList.toggle(`${mainClass}`);
+}
+
+// FOR SECTION PROGRAM
+const readMoreBtnProgram = document.querySelector('.read-more-btn__program');
+const readLessBtnProgram = document.querySelector('.read-less-btn__program');
+const moreTextProgram = document.querySelector('.read-more-text__program');
+
+readMoreBtnProgram.addEventListener('click', onReadMoreBtnProgramClick);
+readLessBtnProgram.addEventListener('click', onReadLessBtnProgramClick);
+
+function onReadMoreBtnProgramClick() {
+  moreTextProgram.classList.toggle(`${mainClass}`);
+  readMoreBtnProgram.classList.toggle(`${mainClass}`);
+  readLessBtnProgram.classList.toggle(`${mainClass}`);
+}
+
+function onReadLessBtnProgramClick() {
+  moreTextProgram.classList.toggle(`${mainClass}`);
+  readMoreBtnProgram.classList.toggle(`${mainClass}`);
+  readLessBtnProgram.classList.toggle(`${mainClass}`);
 }
