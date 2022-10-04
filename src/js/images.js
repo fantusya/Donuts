7a4cc3bf91f688d624d37123f8f43bbc2caadde7
Anastasiia <guba.anastaysha@gmail.com>
2022-10-04 21:40:21 +0300

    file is updated

--- src/js/images.js
+++
@@ -1,6 +0,0 @@
-// const imgArray =[]
-const Li = document.querySelector('.event-info__item');
-console.log(Li);
-
-// Li.style.backgroundImage = 'url(`/src/images/program/donut-1.png`)';
-Li.style.backgroundImage = 'url("donut-1.png")';
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.reviews__slider', {
   // Optional parameters
   loop: true,
   grabCursor: true,
@@ -15,68 +15,38 @@ const swiper = new Swiper('.swiper', {
     type: 'fraction',
   },
 
-  // autoplay: {
-  //   delay: 4000,
-  //   pauseOnMouseEnter: true,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 4000,
+    pauseOnMouseEnter: true,
+    disableOnInteraction: false,
+  },
 
   effect: 'creative',
-  // creativeEffect: {
-  //   prev: {
-  //     // will set `translateZ(-400px)` on previous slides
-  //     translate: [-250, -100, 0],
-  //     scale: 0.5,
-  //   },
-  //   next: {
-  //     // will set `translateX(100%)` on next slides
-  //     translate: [250, -100, 0],
-  //     scale: 0.5,
-  //   },
-  // },
+  creativeEffect: {
+    prev: {
+      shadow: false,
+      translate: ['-125%', 0, -800],
+      rotate: [0, 0, -90],
+    },
+    next: {
+      shadow: false,
+      translate: ['125%', 0, -800],
+      rotate: [0, 0, 90],
+    },
+  },
 
   breakpoints: {
     320: {
-      effect: 'flip',
-      flipEffect: {
-        slideShadows: false,
-      },
       spaceBetween: 60,
       speed: 900,
     },
 
     768: {
-      effect: 'creative',
-      creativeEffect: {
-        prev: {
-          // will set `translateZ(-400px)` on previous slides
-          translate: [-250, -100, 0],
-          scale: 0.5,
-        },
-        next: {
-          // will set `translateX(100%)` on next slides
-          translate: [250, -100, 0],
-          scale: 0.5,
-        },
-      },
       spaceBetween: 60,
       speed: 1000,
     },
 
     1280: {
-      effect: 'creative',
-      creativeEffect: {
-        prev: {
-          // will set `translateZ(-400px)` on previous slides
-          translate: [-250, -100, 0],
-          scale: 0.5,
-        },
-        next: {
-          // will set `translateX(100%)` on next slides
-          translate: [250, -100, 0],
-          scale: 0.5,
-        },
-      },
       spaceBetween: 60,
       speed: 1000,
     },
